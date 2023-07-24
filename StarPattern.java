edd343b9ed9f68dcea7f5a41c94584464f071ba2
Morgaine <54818666+MorgaineLawless@users.noreply.github.com>
2023-07-24 10:29:30 -0400

    Create StarPattern.java

---
+++ StarPattern.java
@@ -0,0 +1,28 @@
+//2.4: Star Pattern
+//Write a program that displays the following pattern:
+
+//   *
+//  ***
+// *****
+//*******
+// *****
+//  ***
+//   *
+
+//Output. Seven lines of output as follows: The first consists of 3 spaces followed by a star. The second line consists of 2 spaces followed by a 3 stars. The third consists of one space followed by 5 stars, and the fourth consists just of 7 stars. The fifth line is identical to third, th sixth to the second and the seventh to the first.
+
+//CLASS NAMES. Your program class should be called StarPattern
+
+public class StarPattern{
+  
+  public static void main(String[] args){
+
+    System.out.print("   *\n");
+    System.out.print("  ***\n");
+    System.out.print(" *****\n");
+    System.out.print("*******\n");
+    System.out.print(" *****\n");
+    System.out.print("  ***\n");
+    System.out.print("   *\n");
+  }
+}
